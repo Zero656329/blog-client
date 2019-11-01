ebdf07c58641fb9a7d1b9755cb91231dacd050f0
韩文杰a <hanwja@fotile.com>
2019-11-01 16:40:05 +0800

    blog-client

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'blog-client/dist/',
     proxyTable: {},
 
     // Various Dev Server settings
