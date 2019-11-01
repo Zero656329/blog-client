07a9e21dfc5a23dd57baff099cb8472cb6b82b83
韩文杰a <hanwja@fotile.com>
2019-11-01 08:56:18 +0800

    blog-client

---
+++ src/components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+ <footer>
+  <p>@xiedaimala.com 2018 wechat:xiedaimala01</p>
+
+ </footer>
+</template>
+<style scope>
+  footer{
+    align-self: end;
+    background-color: #d7d7d7;
+    color: #666;
+    font-size: 13px;
+    padding: 10px;
+    text-align: center;
+  }
+</style>
